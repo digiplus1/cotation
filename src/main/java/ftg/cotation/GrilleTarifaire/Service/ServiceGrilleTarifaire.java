a143f0e0f71e7c1b49451283f4083da1f5f265ef
fokou87 <ghislain.fokou@digiplusconsulting.com>
2021-07-29 09:17:56 +0100

    point specifique

--- src/main/java/ftg/cotation/GrilleTarifaire/Service/ServiceGrilleTarifaire.java
+++ src/main/java/ftg/cotation/GrilleTarifaire/Service/ServiceGrilleTarifaire.java
@@ -1,11 +1,13 @@
 package ftg.cotation.GrilleTarifaire.Service;
 
+import ftg.cotation.Erreur.ErrorMessages;
 import ftg.cotation.GrilleTarifaire.Dao.DaoGrilleTairifaire;
 import ftg.cotation.GrilleTarifaire.Metier.MetierGrilleTarifaire;
 import ftg.cotation.GrilleTarifaire.Model.GrilleTarirefaire;
 import ftg.cotation.GrilleTarifaire.Web.Dto.DtoGrilleTarifaire;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +23,8 @@ public class ServiceGrilleTarifaire implements MetierGrilleTarifaire {
     public GrilleTarirefaire save(DtoGrilleTarifaire dtoGrilleTarifaire) {
         GrilleTarirefaire grilleTarirefaire=new GrilleTarirefaire();
         BeanUtils.copyProperties(dtoGrilleTarifaire,grilleTarirefaire);
+        if (grilleTarirefaire.getDistancemin()>= grilleTarirefaire.getDistancemax()) throw new ErrorMessages("la distance minimale ne peut etre supperieur à la distance maximale", HttpStatus.CONFLICT);
+        if (daoGrilleTairifaire.getGrilleTarirefaireBy(grilleTarirefaire.getDistancemin())!=null)throw new ErrorMessages("Les intervalles doivent etre disjoints", HttpStatus.CONFLICT);
         return daoGrilleTairifaire.save(grilleTarirefaire);
     }
 
