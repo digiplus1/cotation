35cf2c82632019058a1776f44d804072b1fa07f5
fokou87 <ghislain.fokou@digiplusconsulting.com>
2021-07-31 10:11:23 +0100

    first

--- src/main/java/ftg/cotation/GrilleTarifaire/Metier/MetierGrilleTarifaire.java
+++ src/main/java/ftg/cotation/GrilleTarifaire/Metier/MetierGrilleTarifaire.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface MetierGrilleTarifaire {
 
+
     GrilleTarirefaire save(DtoGrilleTarifaire dtoGrilleTarifaire);
     List<GrilleTarirefaire> getAll();
     GrilleTarirefaire updategrille(DtoGrilleTarifaire dtoGrilleTarifaire,Long idgrille);
